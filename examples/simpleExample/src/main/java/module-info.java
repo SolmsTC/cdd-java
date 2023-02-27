3f0f60d32e69f766df2ee9a657cdc96eb91387c1
fritz <fritz@solms.co.za>
2023-02-27 14:09:39 +0200

    commented out the sample module spec to make it runnable - this is only until we figures out how to get Annotation processors to behave nicely with modular code"

--- examples/simpleExample/src/main/java/module-info.java
+++ examples/simpleExample/src/main/java/module-info.java
@@ -1,11 +1,13 @@
-module org.urdad.cdd.examples.simpleExample 
-{
-  requires java.compiler;
-  
-  requires org.urdad.cdd.contract;
-  requires org.urdad.cdd.exceptions;
-  requires org.urdad.cdd.contractValidatingInterceptor;
-  
-  exports org.urdad.cdd.examples.simpleExample;
-  requires com.google.auto.service;
-}
+// Commented out until we figured out how to use Annotation processors with Java modules
+
+//module org.urdad.cdd.examples.simpleExample 
+//{
+//  requires java.compiler;
+//  
+//  requires org.urdad.cdd.contract;
+//  requires org.urdad.cdd.exceptions;
+//  requires org.urdad.cdd.contractValidatingInterceptor;
+//  
+//  exports org.urdad.cdd.examples.simpleExample;
+//  requires com.google.auto.service;
+//}
